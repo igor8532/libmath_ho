545af05b429268c3edb9019587c978c3c067d07e
Igor <zvuash-13@yandex.ru>
2026-06-20 17:25:12 +0000

    correction

--- include/libmath.h
+++ include/libmath.h
@@ -84,19 +84,22 @@ int division(int a, int b, int& c)
 int power(int a, int b, int& c)
 {
     // Отрицательная степень для целых чисел не поддерживается
-    if (b < 0) {
+    if (b < 0)
+    {
         return -3;
     }
 
     // Ситуация, когда a == 0 и b == 0 — математически не определена
-    if (a == 0 && b == 0) {
+    if (a == 0 && b == 0)
+    {
         return -4;
     }
-
     c = 1;
-    for (int i = 0; i < b; ++i) {
+    for (int i = 0; i < b; ++i)
+    {
         // Проверка на переполнение
-        if (c > INT_MAX / a) {
+        if (c > INT_MAX / a)
+       	{
             return -2;
         }
         c = c * a;
@@ -107,12 +110,14 @@ int power(int a, int b, int& c)
 int factorial(int a, int& c)
 {
     // Отрицательное число
-    if (a < 0) {
+    if (a < 0)
+    {
         return -5;
     }
 
     // 0! = 1, 1! = 1
-    if (a <= 1) {
+    if (a <= 1)
+    {
         c = 1;
         return 0;
     }
@@ -125,10 +130,13 @@ int factorial(int a, int& c)
 	return status;
     }
     
-    if (sub_result > INT_MAX / a) {
+    if (sub_result > INT_MAX / a)
+    {
         return -2; // Переполнение
     }
 
     c = sub_result * a;
     return 0;
 }
+
+} // namespace libmath
